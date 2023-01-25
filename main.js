53aded8795c9dea289023f5dcd005155b62bd151
Pavel <chevisap@gmail.com>
2023-01-26 02:41:49 +0300

    added closure function 'sum'

--- main.js
+++ main.js
@@ -1,41 +1,6 @@
-function palindrome(str) {
-  for (let i = 0; i < str.length / 2; i++) {
-    if (str[i] !== str[str.length - 1 - i]) return false;
-  }
-  return true;
+function sum(a) {
+  return function (b) {
+    return a + b;
+  };
 }
-console.log(palindrome("шалаш"));
-
-function min(a, b) {
-  if (a < b) return a;
-  return b;
-}
-console.log(min(1, 2));
-
-function max(a, b) {
-  if (a < b) return b;
-  return a;
-}
-console.log(max(1, 2));
-
-function minMax(a, b) {
-  return a < b ? a : b;
-}
-console.log(minMax(1, 2));
-
-function zero(array) {
-  const result = [];
-
-  for (const number of array) {
-    let newWord = "";
-    const word = number.toString();
-
-    for (const symbol of word) {
-      if (+symbol) newWord += symbol;
-      else newWord += "zero";
-    }
-    result.push(newWord);
-  }
-  return result;
-}
-console.log(zero([0, 12, 10, 43, 40, 99, 100, 13, 31, 30]));
+console.log(sum(5)(2));
