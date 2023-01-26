09a9f55c49fd9549b5abf9c4398dbd115a5ed692
Pavel <chevisap@gmail.com>
2023-01-26 04:53:02 +0300

    added 'getColor' function

--- main.js
+++ main.js
@@ -4,3 +4,30 @@ function sum(a) {
   };
 }
 console.log(sum(5)(2));
+
+function getColor() {
+  const colors = ["magenta", "cyan", "firebrick", "springgreen", "skyblue"];
+  let i = 0;
+  return function () {
+    i === colors.length - 1 ? (i = 0) : i++;
+    return colors[i];
+  };
+}
+
+const getColorText1 = getColor();
+const getColorText2 = getColor();
+const getColorText3 = getColor();
+
+const text1 = document.getElementById("text1");
+const text2 = document.getElementById("text2");
+const text3 = document.getElementById("text3");
+
+text1.addEventListener("click", (event) => {
+  event.target.style.color = getColorText1();
+});
+text2.addEventListener("click", (event) => {
+  event.target.style.color = getColorText2();
+});
+text3.addEventListener("click", (event) => {
+  event.target.style.color = getColorText3();
+});
