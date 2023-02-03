bded761aa93eb3156a7428634bc2ea81ff4af3f3
Pavel <chevisap@gmail.com>
2023-02-03 04:09:26 +0300

    changes after review

--- main.js
+++ main.js
@@ -1,40 +1,39 @@
-function palindrome(str) {
+const palindrome = (str) => {
   for (let i = 0; i < str.length / 2; i++) {
-    if (str[i] !== str[str.length - 1 - i]) return false;
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
   }
   return true;
-}
+};
 console.log(palindrome("шалаш"));
 
 function min(a, b) {
-  if (a < b) return a;
+  if (a < b) {
+    return a;
+  }
   return b;
 }
 console.log(min(1, 2));
 
 function max(a, b) {
-  if (a < b) return b;
+  if (a < b) {
+    return b;
+  }
   return a;
 }
 console.log(max(1, 2));
 
-function minMax(a, b) {
-  return a < b ? a : b;
-}
-console.log(minMax(1, 2));
+const minArrow = (a, b) => (a < b ? a : b);
+console.log(minArrow(12, 22));
+
+const maxArrow = (a, b) => (a > b ? a : b);
+console.log(maxArrow(12, 22));
 
 function zero(array) {
   const result = [];
-
   for (const number of array) {
-    let newWord = "";
-    const word = number.toString();
-
-    for (const symbol of word) {
-      if (+symbol) newWord += symbol;
-      else newWord += "zero";
-    }
-    result.push(newWord);
+    result.push(String(number).replaceAll("0", "zero"));
   }
   return result;
 }
