3f4433a386fe4236de135db705d76338f9f5fb3f
Pavel <chevisap@gmail.com>
2023-02-07 03:04:58 +0300

    added function for date format convetration

--- main.js
+++ main.js
@@ -1,38 +1,7 @@
-function sum(a) {
-  return function (b) {
-    return a + b;
-  };
-}
-console.log(sum(5)(2));
-
-function getColor() {
-  const colors = ["magenta", "cyan", "firebrick", "springgreen", "skyblue"];
-  let i = 0;
-  return function () {
-    i === colors.length - 1 ? (i = 0) : i++;
-    return colors[i];
-  };
+function replacer(match, p1, p2, p3) {
+  return [p3, p2, p1].join(`.`);
 }
 
-module.exports = { getColor, sum };
+const dateFormat = (date) => date.replace(/(\w+).(\w+).(\w+)/, replacer);
 
-const getColorText1 = getColor();
-const getColorText2 = getColor();
-const getColorText3 = getColor();
-
-const text1 = document.getElementById("text1");
-const text2 = document.getElementById("text2");
-const text3 = document.getElementById("text3");
-
-if (text1)
-  text1.addEventListener("click", (event) => {
-    event.target.style.color = getColorText1();
-  });
-if (text2)
-  text2.addEventListener("click", (event) => {
-    event.target.style.color = getColorText2();
-  });
-if (text3)
-  text3.addEventListener("click", (event) => {
-    event.target.style.color = getColorText3();
-  });
+console.log(dateFormat(`2023-12-31`));
