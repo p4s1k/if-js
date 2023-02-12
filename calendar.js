bf26b51239aab6f07b5480bfbb60f7e9e8ea95a0
Pavel <chevisap@gmail.com>
2023-02-13 01:57:37 +0300

    refactoring calendar funtcion

--- calendar.js
+++ calendar.js
@@ -1,53 +1,35 @@
 const getCalendarMonth = (daysInMonth, daysInWeek, dayOfWeek) => {
-  dayOfWeek -= 1;
-
-  if (dayOfWeek >= daysInWeek || dayOfWeek < 0) {
-    return `ERROR day of week "${dayOfWeek + 1}" does not exist`;
+  if (dayOfWeek > daysInWeek || dayOfWeek < 1) {
+    return `ERROR day of week "${dayOfWeek}" does not exist`;
   }
 
   const calendar = [];
-  const arrayOfDaysInMouth = [];
-  const weeksInMouth = Math.floor(daysInMonth / daysInWeek);
+  const weeksInMonth = Math.floor(daysInMonth / daysInWeek);
+  const daysArray = [];
 
-  const addDay = (week) => calendar[week].push(arrayOfDaysInMouth.pop());
+  dayOfWeek -= 1;
 
-  for (let i = daysInMonth; i > 0; i--) {
-    arrayOfDaysInMouth.push(i);
+  for (let i = 1; i <= daysInMonth; i++) {
+    daysArray.push(i);
   }
 
-  for (let week = 0; week <= weeksInMouth; week++) {
-    calendar.push([]);
+  let modifyDaysArray = [...daysArray];
 
-    if (week === 0) {
-      for (let i = 0; i < dayOfWeek; i++) {
-        calendar[week].unshift(daysInMonth - i);
-      }
+  if (dayOfWeek !== 0) {
+    daysArray.reverse();
+    modifyDaysArray = [
+      ...daysArray.slice(0, dayOfWeek).reverse().concat(modifyDaysArray),
+    ];
+    daysArray.reverse();
+  }
 
-      for (let i = 0; i < daysInWeek - dayOfWeek; i++) {
-        addDay(week);
-      }
+  if (modifyDaysArray.length % daysInWeek !== 0) {
+    modifyDaysArray = modifyDaysArray.concat(daysArray.slice(0, daysInWeek));
+  }
 
-      continue;
-    }
-    if (week === weeksInMouth) {
-      if (arrayOfDaysInMouth.length === 0) {
-        calendar.pop();
-        break;
-      }
-
-      for (let i = 0; arrayOfDaysInMouth.length > 0; i++) {
-        addDay(week);
-      }
-
-      let day = 1;
-
-      for (let i = calendar[week].length; i < daysInWeek; i++) {
-        calendar[week].push(day++);
-      }
-    } else {
-      for (let day = 0; day < daysInWeek; day++) {
-        addDay(week);
-      }
+  for (let week = 0; week <= weeksInMonth; week++) {
+    if (modifyDaysArray.length !== 0) {
+      calendar.push(modifyDaysArray.splice(0, daysInWeek));
     }
   }
 
@@ -55,7 +37,7 @@ const getCalendarMonth = (daysInMonth, daysInWeek, dayOfWeek) => {
 };
 
 const daysInMonth = 30;
-const daysInWeek = 10;
+const daysInWeek = 7;
 const dayOfWeek = 5;
 const calendarMonth = getCalendarMonth(daysInMonth, daysInWeek, dayOfWeek);
 
