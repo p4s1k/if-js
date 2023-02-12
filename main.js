b1995b5ebcbcb8e7b85d6be55f9858c2bd590cfd
Pavel <chevisap@gmail.com>
2023-02-12 17:12:32 +0300

    added search function

--- main.js
+++ main.js
@@ -1,3 +1,59 @@
 const palindrome = (str) => str === str.split("").reverse().join("");
 
 console.log(palindrome("civic"));
+
+const data = [
+  {
+    country: "Russia",
+    city: "Saint Petersburg",
+    hotel: "Hotel Leopold",
+  },
+  {
+    country: "Spain",
+    city: "Santa Cruz de Tenerife",
+    hotel: "Apartment Sunshine",
+  },
+  {
+    country: "Slowakia",
+    city: "Vysokie Tatry",
+    hotel: "Villa Kunerad",
+  },
+  {
+    country: "Germany",
+    city: "Berlin",
+    hotel: "Hostel Friendship",
+  },
+  {
+    country: "Indonesia",
+    city: "Bali",
+    hotel: "Ubud Bali Resort&SPA",
+  },
+  {
+    country: "Netherlands",
+    city: "Rotterdam",
+    hotel: "King Kong Hostel",
+  },
+  {
+    country: "Marocco",
+    city: "Ourika",
+    hotel: "Rokoko Hotel",
+  },
+  {
+    country: "Germany",
+    city: "Berlin",
+    hotel: "Hotel Rehberge Berlin Mitte",
+  },
+];
+
+const search = (word) =>
+  data
+    .filter((elem) => {
+      for (const key in elem) {
+        if (elem[key].includes(word)) {
+          return elem;
+        }
+      }
+    })
+    .map((elem) => [elem.country, elem.city, elem.hotel].join(`,`));
+
+console.log(search("Germany"));
