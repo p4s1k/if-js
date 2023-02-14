d3c18fc05954da38d9233ae47988427ea114ad56
Pavel <chevisap@gmail.com>
2023-02-15 00:42:55 +0300

    lesson-4: fix remarks

--- main.js
+++ main.js
@@ -1,37 +1,22 @@
-export const sum = (a) => {
-  return (b) => a + b;
-};
+export const sum = (a) => (b) => a + b;
+
 console.log(sum(5)(2));
 
-export function getColor() {
-  const colors = ["magenta", "cyan", "firebrick", "springgreen", "skyblue"];
-  let i = 0;
-  return function () {
-    i === colors.length - 1 ? (i = 0) : i++;
-    return colors[i];
-  };
-}
+const colors = ["magenta", "cyan", "firebrick", "springgreen", "skyblue"];
 
-const getColorText1 = getColor();
-const getColorText2 = getColor();
-const getColorText3 = getColor();
+export const getColor = () => {
+  let i = 0;
+
+  return (event) => {
+    event.target.style.color= colors[i]
+    i === colors.length - 1 ? (i = 0) : i++;
+  };
+};
 
 const text1 = document.getElementById("text1");
 const text2 = document.getElementById("text2");
 const text3 = document.getElementById("text3");
 
-if (text1) {
-  text1.addEventListener("click", (event) => {
-    event.target.style.color = getColorText1();
-  });
-}
-if (text2) {
-  text2.addEventListener("click", (event) => {
-    event.target.style.color = getColorText2();
-  });
-}
-if (text3) {
-  text3.addEventListener("click", (event) => {
-    event.target.style.color = getColorText3();
-  });
-}
+text1.addEventListener("click", getColor())
+text2.addEventListener("click", getColor())
+text3.addEventListener("click", getColor())
