410ee25ebdda753a279bb5ca58b27eab533cd249
Pavel <chevisap@gmail.com>
2023-02-16 04:57:05 +0300

    lesson-6: refactor calendar function

--- calendar.js
+++ calendar.js
@@ -1,44 +1,32 @@
 const getCalendarMonth = (daysInMonth, daysInWeek, dayOfWeek) => {
-  if (dayOfWeek > daysInWeek || dayOfWeek < 1) {
-    return `ERROR day of week "${dayOfWeek}" does not exist`;
+  if (dayOfWeek > daysInWeek) {
+    return `ERROR day of week ${dayOfWeek} does not exist`;
   }
 
   const calendar = [];
-  const weeksInMonth = Math.floor(daysInMonth / daysInWeek);
-  const daysArray = [];
-
-  dayOfWeek -= 1;
-
-  for (let i = 1; i <= daysInMonth; i++) {
-    daysArray.push(i);
-  }
-
-  let modifyDaysArray = [...daysArray];
+  const weeksInMonth = Math.ceil((daysInMonth + dayOfWeek) / daysInWeek);
+  let day = 1;
 
   if (dayOfWeek !== 0) {
-    daysArray.reverse();
-    modifyDaysArray = [
-      ...daysArray.slice(0, dayOfWeek).reverse().concat(modifyDaysArray),
-    ];
-    daysArray.reverse();
+    day = daysInMonth - dayOfWeek + 1;
   }
 
-  if (modifyDaysArray.length % daysInWeek !== 0) {
-    modifyDaysArray = modifyDaysArray.concat(daysArray.slice(0, daysInWeek));
-  }
-
-  for (let week = 0; week <= weeksInMonth; week++) {
-    if (modifyDaysArray.length !== 0) {
-      calendar.push(modifyDaysArray.splice(0, daysInWeek));
+  for (let i = 0; i < weeksInMonth; i++) {
+    calendar.push([]);
+    for (let j = 0; j < daysInWeek; j++) {
+      if (day > daysInMonth) {
+        day = 1;
+      }
+      calendar[i].push(day++);
     }
   }
 
   return calendar;
 };
 
-const daysInMonth = 30;
+const daysInMonth = 31;
 const daysInWeek = 7;
-const dayOfWeek = 5;
+const dayOfWeek = 6;
 const calendarMonth = getCalendarMonth(daysInMonth, daysInWeek, dayOfWeek);
 
 console.log(calendarMonth);
