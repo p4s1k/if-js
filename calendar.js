bcf91749146da4508fb2a366d51f545b6d7a1c35
Pavel <chevisap@gmail.com>
2023-02-20 00:38:45 +0300

    lesson-7: add new functionality to a calendar function

--- calendar.js
+++ calendar.js
@@ -1,4 +1,17 @@
-const getCalendarMonth = (daysInMonth, daysInWeek, dayOfWeek) => {
+const getCalendarMonth = (
+  daysInMonth,
+  daysInWeek,
+  dayOfWeek,
+  checkInDate,
+  checkOutDate
+) => {
+  const objectOfDay = {
+    dayOfMonth: 28, // required field - день месяца
+    notCurrentMonth: false, // optional field - день не входит в текущий месяц
+    selectedDay: false, // optional field - выбранный пользователем день
+    dayOfStay: false, // optional field - день прибывания
+  };
+
   if (dayOfWeek > daysInWeek) {
     return `ERROR day of week ${dayOfWeek} does not exist`;
   }
@@ -8,16 +21,27 @@ const getCalendarMonth = (daysInMonth, daysInWeek, dayOfWeek) => {
   let day = 1;
 
   if (dayOfWeek !== 0) {
+    objectOfDay.notCurrentMonth = !objectOfDay.notCurrentMonth;
     day = daysInMonth - dayOfWeek + 1;
   }
 
   for (let i = 0; i < weeksInMonth; i++) {
     calendar.push([]);
+
     for (let j = 0; j < daysInWeek; j++) {
       if (day > daysInMonth) {
+        objectOfDay.notCurrentMonth = !objectOfDay.notCurrentMonth;
         day = 1;
       }
-      calendar[i].push(day++);
+
+      objectOfDay.dayOfStay =
+        objectOfDay.notCurrentMonth === false &&
+        checkInDate <= day &&
+        checkOutDate >= day;
+
+      objectOfDay.dayOfMonth = day++;
+
+      calendar[i].push({ ...objectOfDay });
     }
   }
 
@@ -26,7 +50,15 @@ const getCalendarMonth = (daysInMonth, daysInWeek, dayOfWeek) => {
 
 const daysInMonth = 31;
 const daysInWeek = 7;
-const dayOfWeek = 6;
-const calendarMonth = getCalendarMonth(daysInMonth, daysInWeek, dayOfWeek);
+const dayOfWeek = 4;
+const checkInDate = 1;
+const checkOutDate = 5;
+const calendarMonth = getCalendarMonth(
+  daysInMonth,
+  daysInWeek,
+  dayOfWeek,
+  checkInDate,
+  checkOutDate
+);
 
 console.log(calendarMonth);
