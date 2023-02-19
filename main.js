1e67028aa5847adc5cb2175a4e7a1387849b38ac
Pavel <chevisap@gmail.com>
2023-02-19 21:00:53 +0300

    вувуdelete comments

--- main.js
+++ main.js
@@ -33,25 +33,18 @@ const obj3 = {
 
 const deepEqual = (obj1, obj2) => {
   if (Object.keys(obj1).length !== Object.keys(obj2).length) {
-    //ЕСЛИ РАЗНОЕ КОЛИЧЕСТВО КЛЮЧЕЙ, ТО СРАЗУ БАН
     return false;
   }
   for (const objKey in obj1) {
     if (obj2[objKey] === undefined) {
-      //ЕСЛИ НЕТ КЛЮЧА У ВТОРОГО ОБЪЕКТА, ТО БАН
       return false;
     }
-    //БЕЖИМ ПО ОБЪЕКТУ
     if (typeof obj1[objKey] === "object" && typeof obj2[objKey] === "object") {
-      // ЕСЛИ ЗНАЧЕНИЕ - ЭТО ОБЪЕКТ, ТО ЗАХОДИМ В НЕГО И ОЖИДАЕМ TRUE ДЛЯ ПОДОЛЖЕНИЯ
       if (!deepEqual(obj1[objKey], obj2[objKey])) {
-        //ЕСЛИ ВЛОЖЕННЫЙ ОБЪЕКТ ПРОВЕРКУ НЕ ПРОШЕЛ, ТО МЫ С НИМ В ТУР НЕ ПОЕХАЛИ
         return false;
       }
     } else {
-      // ЕСЛИ НЕ ОБЪЕКТ, ТО СРАВНИЕВАЕМ
       if (obj1[objKey] !== obj2[objKey]) {
-        // РАЗНЫЕ ЗНАЧЕНИЯ - БАН
         return false;
       }
     }
