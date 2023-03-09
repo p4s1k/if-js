f361160b4e1c6eb5ccf89f8fd1e3f8e588e83c6d
Pavel <chevisap@gmail.com>
2023-03-10 02:34:03 +0300

    lesson-9: work with Symbol.iterator

--- main.js
+++ main.js
@@ -1,30 +1,33 @@
-import Students from "./Students.js";
+const colors = {
+  data: ["magenta", "cyan", "firebrick", "springgreen", "skyblue"],
+  map: new Map(),
+  [Symbol.iterator]() {
+    return this;
+  },
+  next(id) {
+    if (!this.map.has(id)) {
+      this.map.set(id, -1);
+    }
 
-const studentsData = [
-  {
-    firstName: "Василий",
-    lastName: "Петров",
-    admissionYear: 2019,
-    courseName: "Java",
-  },
-  {
-    firstName: "Иван",
-    lastName: "Иванов",
-    admissionYear: 2018,
-    courseName: "JavaScript",
-  },
-  {
-    firstName: "Александр",
-    lastName: "Федоров",
-    admissionYear: 2017,
-    courseName: "Python",
-  },
-  {
-    firstName: "Николай",
-    lastName: "Петров",
-    admissionYear: 2019,
-    courseName: "Android",
-  },
-];
+    this.map.get(id) === this.data.length - 1
+      ? this.map.set(id, 0)
+      : this.map.set(id, this.map.get(id) + 1);
 
-console.log(new Students({ students: studentsData }).getInfo());
+    return {
+      done: true,
+      value: this.data[this.map.get(id)],
+    };
+  },
+};
+
+const getColor = (id) => (event) => {
+  event.target.style.color = colors.next(id).value;
+};
+
+const text1 = document.getElementById("text1");
+const text2 = document.getElementById("text2");
+const text3 = document.getElementById("text3");
+
+text1.addEventListener("click", getColor(text1.id));
+text2.addEventListener("click", getColor(text2.id));
+text3.addEventListener("click", getColor(text3.id));
